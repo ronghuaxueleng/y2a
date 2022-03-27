b44b15ca05b992139b9eb5ae35b0d410e7fd0498
caoqiang <caoqiang@jiuqi.com.cn>
2022-03-27 15:23:39 +0800

    up

--- streams.py
+++ streams.py
@@ -7,6 +7,6 @@ logger = logging.getLogger(__name__)
 
 
 class Stream(StreamBase):
-    def stream_to_raw(self, proxies) -> bytes:
+    def stream_to_raw(self, proxies) -> None:
         response = requests.get(self.url, proxies=proxies, stream=True)
-        return response.content
+        return response.raw
