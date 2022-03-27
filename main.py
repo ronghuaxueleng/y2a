0dc461958b967fa0a133cce65b930fddd8cc61a1
caoqiang <caoqiang@jiuqi.com.cn>
2022-03-27 15:14:11 +0800

    up

--- main.py
+++ main.py
@@ -11,5 +11,4 @@ if __name__ == '__main__':
 
     ali = Aligo(refresh_token="d816542f503f4d8f967fc4cba2c086e7")
     up_file = ali.upload_file_from_youtube('http://youtube.com/watch?v=9bZkp7q19f0', proxies=proxies)
-    ali.upload_file()
     print(up_file)
